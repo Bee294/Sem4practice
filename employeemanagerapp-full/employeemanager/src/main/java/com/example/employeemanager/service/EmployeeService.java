32f521414e8e88bf8245a13bb0b7ca1ce65e21b1
Đỗ Thế Đức <ducdtth2108036@fpt.edu.vn>
2023-08-07 19:16:13 +0700

    exam

---
+++ employeemanagerapp-full/employeemanager/src/main/java/com/example/employeemanager/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.example.employeemanager.service;
+
+import com.example.employeemanager.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface EmployeeService {
+    public Employee addEmployee(Employee employee);
+    public List<Employee> findAllEmployees();
+    public Employee updateEmployee(Employee employee);
+    public Employee findEmployeeById(Long id);
+    public void deleteEmployee(Long id);
+}
