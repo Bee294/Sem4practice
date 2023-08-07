54554006d39e41a9505fd70e88aa21692dbe5322
Đỗ Thế Đức <ducdtth2108036@fpt.edu.vn>
2023-08-07 09:40:16 +0700

    rm

--- employeemanagerapp-full/employeemanager/src/main/java/com/example/employeemanager/common/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeemanager.common;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
